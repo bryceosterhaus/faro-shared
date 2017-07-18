b25aef91ec4010990354b05e21f5d17ad7d19036
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2017-07-18 11:06:55 -0700

    Update

---
+++ src/Icon.js
@@ -0,0 +1,11 @@
+import Component, {Config} from 'metal-jsx';
+
+class Icon extends Component {
+	render() {
+		return (
+			<div>This is an Icon Component</div>
+		);
+	}
+}
+
+export default Icon;
